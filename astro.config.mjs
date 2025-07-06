9871485e2753610648dc04035423935bd12509d7
Feiyang Wu <feiyangwu@gatech.edu>
2025-07-06 13:58:45 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   // Replace with your GitHub Pages URL
   // For user/organization sites: https://<username>.github.io
   // For project sites: https://<username>.github.io/<repository-name>
-  site: 'https://fei-yang-wu.github.io',
+  site: "https://fei-yang-wu.github.io",
 
   // If deploying to a project repository, uncomment and set the base path
-  // base: '/<repository-name>',
-  output: 'static',
+  base: "/fei-yang-wu",
+  output: "static",
 
   integrations: [mdx(), sitemap()],
 
